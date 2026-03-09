49d53334a7dadddd1ca697ea444b66a991d4a4d9
Archit <aryalarchit9@gmail.com>
2026-03-09 19:09:31 +0545

    Insert initial data for Student, Club, and Membership

--- Task3-Database/insert_data.sql
+++ Task3-Database/insert_data.sql
@@ -1,1 +1,11 @@
+INSERT INTO Student VALUES (1,'Asha','asha@email.com');
+INSERT INTO Student VALUES (2,'Bikash','bikash@email.com');
+INSERT INTO Student VALUES (3,'Nisha','nisha@email.com');
 
+INSERT INTO Club VALUES (1,'Music Club','R101','Mr Raman');
+INSERT INTO Club VALUES (2,'Sports Club','R202','Ms Sita');
+INSERT INTO Club VALUES (3,'Drama Club','R303','Mr Kiran');
+
+INSERT INTO Membership VALUES (1,1,1,'2024-01-10');
+INSERT INTO Membership VALUES (2,2,2,'2024-01-12');
+INSERT INTO Membership VALUES (3,1,2,'2024-01-15');
