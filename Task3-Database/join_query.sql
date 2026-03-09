f18b12deb134ee747f8345fc8554b29f0e4e638a
Archit <aryalarchit9@gmail.com>
2026-03-09 19:09:53 +0545

    Add join query to retrieve student club memberships

--- Task3-Database/join_query.sql
+++ Task3-Database/join_query.sql
@@ -1,1 +1,8 @@
-
+SELECT Student.StudentName,
+       Club.ClubName,
+       Membership.JoinDate
+FROM Membership
+JOIN Student
+ON Membership.StudentID = Student.StudentID
+JOIN Club
+ON Membership.ClubID = Club.ClubID;
